2e06afb4d5f00097cfba31195b17000a3dd7abe2
ahaigh9877 <alastairhaigh@me.com>
2020-01-13 16:50:05 +0100

    Rating stars fill black on hover

--- src/components/RateJoke/RateJoke.js
+++ src/components/RateJoke/RateJoke.js
@@ -4,39 +4,34 @@ import hollowStar from "../../Black_star_hollow.svg";
 const RateJoke = props => {
   return (
     <div class="starContainer">
-      <img
+      <div
         className="rateStar"
         id="oneStar"
         onClick={() => props.handleClick(1)}
-        src={hollowStar}
         alt="hollow star"
       />
-      <img
+      <div
         className="rateStar"
         id="twoStar"
         onClick={() => props.handleClick(2)}
-        src={hollowStar}
         alt="hollow star"
       />
-      <img
+      <div
         className="rateStar"
         id="threeStar"
         onClick={() => props.handleClick(3)}
-        src={hollowStar}
         alt="hollow star"
       />
-      <img
+      <div
         className="rateStar"
         id="fourStar"
         onClick={() => props.handleClick(4)}
-        src={hollowStar}
         alt="hollow star"
       />
-      <img
+      <div
         className="rateStar"
         id="fiveStar"
         onClick={() => props.handleClick(5)}
-        src={hollowStar}
         alt="hollow star"
       />
     </div>
